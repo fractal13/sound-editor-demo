7090a1e8e6677e88d39d6ddcd2f0e62036efe2eb
Curtis Larsen <curtis.gale.larsen@gmail.com>
2023-05-16 09:20:49 -0600

    instrument wave shapes

--- trials/mary_refactor.cpp
+++ trials/mary_refactor.cpp
@@ -96,6 +96,129 @@ void attack_and_decline(const double max_amplitude, const double hz,
  * Envelope end
  *******************************************************************/
 
+
+/*******************************************************************
+ * Instrument file start
+ */
+constexpr double two_pi = 6.283185307179586476925286766559;
+
+class Instrument {
+public:
+  Instrument();
+  virtual void generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second);
+private:
+};
+
+Instrument::Instrument() {
+}
+
+void Instrument::generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second) {
+  
+  int N = samples_per_second * seconds;  // total number of samples
+  samples.resize(N);
+  for (int n = 0; n < N; n++) {
+    double value1 = std::sin((two_pi * n * frequency) / samples_per_second);
+    samples[n] = value1;
+  }
+}
+
+class Instrument2: public Instrument {
+public:
+  Instrument2();
+  virtual void generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second);
+private:
+};
+
+Instrument2::Instrument2() 
+  : Instrument() {
+}
+
+void Instrument2::generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second) {
+
+  int N = samples_per_second * seconds;  // total number of samples
+  samples.resize(N);
+  for (int n = 0; n < N; n++) {
+    double value1 = std::cos((two_pi * n * frequency) / samples_per_second);
+    if(value1 > 0.5) {
+      value1 = 1.0;
+    } else if(value1 < -0.5) {
+      value1 = -1.0;
+    }
+    samples[n] = value1;
+  }
+}
+
+class Instrument3: public Instrument {
+public:
+  Instrument3();
+  virtual void generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second);
+private:
+};
+
+Instrument3::Instrument3() 
+  : Instrument() {
+}
+
+void Instrument3::generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second) {
+
+  int N = samples_per_second * seconds;  // total number of samples
+  samples.resize(N);
+  for (int n = 0; n < N; n++) {
+    double angle = (two_pi * n * frequency) / samples_per_second;
+    double position = std::fmod(angle, two_pi)/two_pi;
+    double value = 0.0;
+    if(position < 0.25) {
+      double m = (1.0/0.25);
+      double b = 0.0;
+      double x = (position - 0.0);
+      value = m*x+b;
+    } else if(position < 0.75) {
+      double m = (-2.0/0.5);
+      double b = 1.0;
+      double x = (position - 0.25);
+      value = m*x+b;
+    } else {
+      double m = (1.0/0.25);
+      double b = -1.0;
+      double x = (position - 0.75);
+      value = m*x+b;
+    }
+    samples[n] = value;
+  }
+}
+
+class Instrument4: public Instrument {
+public:
+  Instrument4();
+  virtual void generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second);
+private:
+};
+
+Instrument4::Instrument4() 
+  : Instrument() {
+}
+
+void Instrument4::generateSamples(std::vector<double>& samples, const double frequency, const double seconds, const int samples_per_second) {
+
+  int N = samples_per_second * seconds;  // total number of samples
+  samples.resize(N);
+  for (int n = 0; n < N; n++) {
+    double angle = (two_pi * n * frequency) / samples_per_second;
+    double position = std::fmod(angle, two_pi)/two_pi;
+    double value = 0.0;
+    if(position < 0.5) {
+      value = 1.0;
+    } else {
+      value = -1.0;
+    }
+    samples[n] = value;
+  }
+}
+
+/*
+ * Instrument end
+ *******************************************************************/
+
 /*******************************************************************
  * Wav file start
  */
@@ -159,9 +282,8 @@ void WaveFile::writeDataSubchunkHeader() {
 }
 
 void WaveFile::writeNotes(const std::vector<Note>& notes) {
-  constexpr double two_pi = 6.283185307179586476925286766559;
   constexpr double max_amplitude = 32760;  // "volume"
-
+  Instrument4 instrument;
   //double whole_note_seconds = 4.0*60.0/80.0; // q=80
   double whole_note_seconds = 4.0*60.0/120.0; // q=120
   for(auto& note: notes) {
@@ -174,10 +296,12 @@ void WaveFile::writeNotes(const std::vector<Note>& notes) {
     double decline_min = 0.0;
     std::vector<double> amplitude;
     attack_and_decline(max_amplitude, mSamplesPerSecond, attack_seconds, attack_min, decline_seconds, decline_min, amplitude);
+    std::vector<double> samples;
+    instrument.generateSamples(samples, frequency, seconds, mSamplesPerSecond);
 
     int N = mSamplesPerSecond * seconds;  // total number of samples
     for (int n = 0; n < N; n++) {
-      double value1 = sin((two_pi * n * frequency) / mSamplesPerSecond);
+      double value1 = samples[n];
       double value2 = value1;
       // left channel
       little_endian_io::write_word(mFile, (int)(amplitude[n] * value1), 2);
